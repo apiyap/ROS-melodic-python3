c098dc987c8fe16c160fc64ee86b5949959ec6f5
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-05-25 20:17:56 +0700

    Create ros-ins.sh

---
+++ ros-ins.sh
@@ -0,0 +1,25 @@
+set -e
+echo "Installation"
+echo "Create a catkin Workspace"
+mkdir -p ~/ros_catkin_ws
+cd ~/ros_catkin_ws
+echo "ros install generator .."
+rosinstall_generator $ROS_PKG --rosdistro $ROS_DISTRO --deps --tar > $ROS_DISTRO-$ROS_PKG.rosinstall
+
+wstool init -j4 src $ROS_DISTRO-$ROS_PKG.rosinstall
+echo "Resolving Dependencies"
+sudo apt install -y python3-rosdep
+rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y
+echo "Building the catkin Workspace"
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+source ~/ros_catkin_ws/install_isolated/setup.bash
+echo "Update the workspace"
+mv -i $ROS_DISTRO-$ROS_PKG.rosinstall $ROS_DISTRO-$ROS_PKG.rosinstall.old
+rosinstall_generator $ROS_PKG --rosdistro $ROS_DISTRO --deps --tar > $ROS_DISTRO-$ROS_PKG.rosinstall
+diff -u $ROS_DISTRO-$ROS_PKG.rosinstall $ROS_DISTRO-$ROS_PKG.rosinstall.old
+wstool merge -t src $ROS_DISTRO-$ROS_PKG.rosinstall
+wstool update -t src
+echo "Rebuild your workspace"
+./src/catkin/bin/catkin_make_isolated --install
+echo "source ~/ros_catkin_ws/install_isolated/setup.bash" >> ~/.bashrc
+echo "Build completed"
