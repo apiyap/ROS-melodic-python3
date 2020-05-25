5880e5199c428e48294fab9a765dc57ae8d035cd
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-05-25 20:28:09 +0700

    Update ros-py3.sh

--- ros-py3.sh
+++ ros-py3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+echo "Setup your sources.list"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+echo "Set up your keys"
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
 ROS_PKG=desktop_full  #desktop_full,desktop,ros-base se https://www.ros.org/reps/rep-0131.html#variants
 ROS_DISTRO=melodic
 echo "Preparing.."
