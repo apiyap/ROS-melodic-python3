8e2a0dc4f41cf6ceb77b0842b07bf532bf475766
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-06-05 08:11:48 +0700

    Add files via upload

--- ros-py3-cv4.3.sh
+++ ros-py3-cv4.3.sh
@@ -36,15 +36,9 @@ rosinstall_generator $ROS_PKG --rosdistro $ROS_DISTRO --deps --tar > $ROS_DISTRO
 wstool init -j4 src $ROS_DISTRO-$ROS_PKG.rosinstall
 
 echo "Update patch..."
-patch -N src/vision_opencv/cv_bridge/CMakeLists.txt $WHEREAMI'/patchs/00e0ca81522ce0928f96ce1805abc833ac2de4f7.patch'
-patch -N -d src/vision_opencv/cv_bridge/src/ < $WHEREAMI'/patchs/d8c7bd7717d4348848ef9443c560669d51a5ecf7.patch'
-#patch -N -d src/vision_opencv/cv_bridge/src/ < $WHEREAMI'/patchs/cdbfe885ad0c55a2a78d103f01893d1a15e22ce8.patch'
-patch -N -d src/vision_opencv/cv_bridge/src/ < $WHEREAMI'/patchs/1bab9c83385bf28629a7b244dfa35cd233d8b799.patch'
-
-patch -N src/image_pipeline/image_publisher/CMakeLists.txt $WHEREAMI'/patchs/image_pub_CMakeList.txt.patch'
-patch -N src/image_pipeline/image_publisher/src/nodelet/image_publisher_nodelet.cpp $WHEREAMI'/patchs/image_publisher_nodelet.cpp.patch'
-patch -N src/image_transport_plugins/compressed_depth_image_transport/src/codec.cpp $WHEREAMI'/patchs/compressed_depth_image_transport_src_codec.cpp.patch'
-patch -N src/image_transport_plugins/compressed_image_transport/src/compressed_publisher.cpp $WHEREAMI'/patchs/compressed_image_transport_src_compressed_publisher.cpp.patch'
+cd ~
+patch -N -p1 -d ~/ros_org_ws < $WHEREAMI'/patchs/opencv4.3.0.patch'
+cd ~/ros_catkin_ws
 
 echo "Resolving Dependencies"
 set +e
